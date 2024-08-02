8a50469588c37ba556d2a0e147a52243af2267fd
Joseph Aizawa <josephaizawa@gmail.com>
2024-08-02 14:53:51 -0700

    adds updates for user profile editing

---
+++ src/components/EditProfile/EditProfile.jsx
@@ -0,0 +1,186 @@
+import React from "react";
+import "../EditProfile/EditProfile.scss";
+
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { notification } from "antd";
+
+import BackButton from "../BackButton/BackButton";
+import RestartButton from "../RestartButton/RestartButton";
+
+import axios from "axios";
+
+const EditProfile = ({ setIsEditing }) => {
+  const [user, setUser] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    phone: "",
+    address: "",
+    role: "",
+  });
+  const navigate = useNavigate();
+  const [api, contextHolder] = notification.useNotification();
+
+  const blankFieldNotification = (type) => {
+    api[type]({
+      message: "Field Blank",
+      description:
+        "Email is a required field. Please make sure you've added an Email.",
+    });
+  };
+
+  const emailNotification = (type) => {
+    api[type]({
+      message: "Not a valid email",
+      description: "Please enter a valid email address.",
+    });
+  };
+
+  let handleUserChange = (e) => {
+    let { name, value } = e.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+    console.log(user);
+  };
+
+  const isFormValid = () => {
+    const emailPattern =
+      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+
+    const isEmailValid = () => {
+      if (emailPattern.test(user.email)) {
+        return true;
+      } else {
+        emailNotification("error");
+        return false;
+      }
+    };
+
+    // TO DO: Check if the fields are all filled
+    if (!user.email) {
+      blankFieldNotification("error");
+      return false;
+    }
+    if (!isEmailValid()) {
+      // openNotificationWithIcon("error");
+      return false;
+    }
+    return true;
+  };
+
+  useEffect(() => {
+    const loadData = async () => {
+      const token = sessionStorage.getItem("token");
+
+      try {
+        // Get the data from the API
+        const { data } = await axios.get(
+          "http://localhost:8080/users/profile",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setUser(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    loadData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+    // Here send a POST request to signupUrl with username, name and password data
+    if (isFormValid()) {
+      console.log(user);
+      try {
+        const response = await axios.put(
+          "http://localhost:8080/users/edituser",
+          user
+        );
+        setIsEditing(false);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+  return (
+    <>
+      {contextHolder}
+      <div className="signup-background">
+        <main className="signup__app-window">
+          <nav className="nav">
+            <BackButton />
+            <RestartButton />
+          </nav>
+          <h1 className="signup-form__title">Edit Profile</h1>
+          <form className="signup-form" onSubmit={handleSubmit}>
+            <section className="signup-form__fields">
+              <label className="signup-form__lable">
+                First Name:
+                <input
+                  className="signup-form__input"
+                  type="text"
+                  name="first_name"
+                  onChange={handleUserChange}
+                  value={user.first_name}
+                />
+              </label>
+              <label className="signup-form__lable">
+                Last Name:
+                <input
+                  className="signup-form__input"
+                  type="text"
+                  name="last_name"
+                  onChange={handleUserChange}
+                  value={user.last_name}
+                />
+              </label>
+              <label className="signup-form__lable">
+                Email:
+                <input
+                  className="signup-form__input"
+                  type="text"
+                  name="email"
+                  onChange={handleUserChange}
+                  value={user.email}
+                />
+              </label>
+              <label className="signup-form__lable">
+                Phone:
+                <input
+                  className="signup-form__input"
+                  type="text"
+                  name="phone"
+                  onChange={handleUserChange}
+                  value={user.phone}
+                />
+              </label>
+              <label className="signup-form__lable">
+                Address:
+                <input
+                  className="signup-form__input"
+                  type="text"
+                  name="address"
+                  onChange={handleUserChange}
+                  value={user.address}
+                />
+              </label>
+            </section>
+            <button className="signup-form__submit" type="submit">
+              Save
+            </button>
+          </form>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default EditProfile;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,12 +6,68 @@ import { notification } from "antd";
 // import React from "react";
 import BackButton from "../BackButton/BackButton";
 import RestartButton from "../RestartButton/RestartButton";
+import editButton from "../../assets/icons/edit-24px.svg";
 import axios from "axios";
+import EditProfile from "../EditProfile/EditProfile";
 
 function ProfileComponent() {
   const [user, setUser] = useState(null);
   const [failedAuth, setFailedAuth] = useState(false);
+  const [email, setEmail] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
 
+  const [api, contextHolder] = notification.useNotification();
+
+  const blankFieldNotification = (type) => {
+    api[type]({
+      message: "Field Blank",
+      description: "Please make sure you've added an Email.",
+    });
+  };
+
+  const emailNotification = (type) => {
+    api[type]({
+      message: "Not a valid email",
+      description: "Please enter a valid email address.",
+    });
+  };
+
+  const successNotification = (type) => {
+    api[type]({
+      message: "Welcome to QuickEncounter",
+      description: "You have signed up successfully.",
+    });
+  };
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+
+    const isFormValid = () => {
+      const emailPattern =
+        /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+
+      const isEmailValid = () => {
+        if (emailPattern.test(email)) {
+          return true;
+        } else {
+          emailNotification("error");
+          return false;
+        }
+        return true;
+      };
+
+      // TO DO: Check if the fields are all filled
+      if (!email) {
+        blankFieldNotification("error");
+        return false;
+      }
+      if (!isEmailValid()) {
+        // openNotificationWithIcon("error");
+        return false;
+      }
+      return true;
+    };
+  };
   useEffect(() => {
     const loadData = async () => {
       const token = sessionStorage.getItem("token");
@@ -40,7 +96,7 @@ function ProfileComponent() {
       }
     };
     loadData();
-  }, []);
+  }, [isEditing]);
 
   const handleLogout = () => {
     sessionStorage.removeItem("token");
@@ -79,6 +135,38 @@ function ProfileComponent() {
     );
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Here send a POST request to signupUrl with username, name and password data
+    if (isFormValid()) {
+      successNotification("success");
+      try {
+        const response = await axios.post(
+          "http://localhost:8080/users/signup",
+          {
+            email: email,
+            password: password,
+          }
+        );
+        console.log(response.data);
+        setIsSignedUp(true);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const handleEdit = () => {
+    console.log("button clicked");
+    setIsEditing(true);
+  };
+
+  const renderEdit = () => (
+    <div>
+      <EditProfile setIsEditing={setIsEditing} />
+    </div>
+  );
+  if (isEditing) return renderEdit();
   return (
     <>
       <div className="profile-background">
@@ -92,8 +180,13 @@ function ProfileComponent() {
             <h2 className="profile__welcome">
               Welcome back, {user.first_name} {user.last_name}
             </h2>
-            <h2 className="profile__title">My Profile</h2>
+            <div className="profile__info-header">
+              <h2 className="profile__title">My Profile</h2>
+              <img src={editButton} onClick={handleEdit} />
+            </div>
             <section className="profile__fields">
+              <p className="profile__lable">First Name: {user.first_name}</p>
+              <p className="profile__lable">Last Name: {user.last_name}</p>
               <p className="profile__lable">Email: {user.email}</p>
               <p className="profile__lable">Phone: {user.phone}</p>
               <p className="profile__lable">Address: {user.address}</p>
